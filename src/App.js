f567b2d8ae0f2fd3c1a6a5669584a4c587e82bf6
mrityunjay <rawatmrityunjay21@gmail.com>
2023-02-08 10:48:43 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
+import React, {useState, usestate} from "react"
 import logo from './logo.svg';
 import './App.css';
 
+
+const emoji_dic = {
+
+  "😁":"Smiling",
+  "😳":"Disbelief",
+  "😂":"Laughing",
+  "🥰":"Loved",
+  "😔":"Sad",
+  "🥱":"Yawning",
+  "😱":"Shocked",
+  "😑":"Annoyed",
+  "😡":"Angry",
+  "🤒":"Sick" 
+
+
+
+};
+
+var emojiesWeKnow =  Object.keys(emoji_dic)
+
+
 function App() {
+
+  const [ meaning , setMeaning ] = useState("")
+
+    function emojiInputHandler(event){
+
+      var user_input = event.target.value;
+
+      var meaning = emoji_dic[user_input]
+
+      if (meaning === undefined) {
+
+        meaning =  "We don't have this emoji in our database "
+
+      }
+
+      setMeaning(meaning);
+
+    }
+
+    function emojiClickHandler(emoji){
+
+      var meaning = emoji_dic[emoji]
+
+      setMeaning(meaning);
+
+    }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container" >
+
+
+      <div className="heading" >Know your Emoji</div>
+      
+      <div className=" input-container " >
+
+        <input type="text" id="input" name="input" onChange={emojiInputHandler} ></input>
+
+      </div>
+
+      <div className="output-container" ><h2 id="output" >{ meaning }</h2></div>
+
+      <div className="emoji-container" >
+
+      {
+
+        emojiesWeKnow.map(function(emoji){
+
+          return <span onClick={() => emojiClickHandler(emoji)} key={emoji} >{emoji}</span>
+
+        })
+
+      }
+
+      </div>
+
+
+
     </div>
   );
 }
